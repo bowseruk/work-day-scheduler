56f3aa71308febfb7f4f44af09d28b4ed3248a05
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-01-27 20:59:01 +0000

    got button functionality started

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,20 @@ for (let i = 0; i < 24; i++) {
     let div = $('<div>');
     div.text(`${String(i).padStart(2,'0')}:00`);
     div.addClass("hour");
-    let input = $('<input>');
+    let input = $('<textarea>');
     if (dayjs().get('h') > i) {
-        input.addClass("future");
-    } else if (dayjs().get('h') < i) {
         input.addClass("past");
+    } else if (dayjs().get('h') < i) {
+        input.addClass("future");
     } else {
         input.addClass("present");
     }
-    
+    input.attr("data-hour", i)
+    input.attr("id", `${i}-note`)
     let button = $('<button>');
     button.addClass("saveBtn");
+    button.attr("data-hour", i);
+    button.on("click", saveNote)
     let logo = $('<i>');
     logo.addClass("fa-regular fa-floppy-disk")
     button.append(logo);
@@ -29,4 +32,11 @@ for (let i = 0; i < 24; i++) {
     timeblock.append(button);
     timeblock.addClass('time-block row');
     $('.container').append(timeblock);
+}
+
+function saveNote(event) {
+    console.log($(this).attr("data-hour"));
+    id = $(this).attr("data-hour");
+    value = $(`#${id}-note`).val();
+    console.log(value);
 }
